73b1431399f27a644ee060a17ccd8ff212b5e0a5
zhangyanwei <wardzhang@shunshunliuxue.com>
2016-05-05 16:48:02 +0800

    增加手机格式校验

--- platform/helper/mobile.py
+++ platform/helper/mobile.py
@@ -7,6 +7,11 @@ from . import http
 def get_mobile_attribution(mobile):
     """获取手机归属地
     """
+    mobile_re = re.compile('((^(13\d|14[57]|15[^4,\D]|17[345678]|18\d)'
+                           '\d{8}|170[059]\d{7})(|\+\d))$')
+    if not mobile_re.match(mobile):
+        return {'province': None}
+
     mobile = mobile[:11]
     url = 'http://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel={tel}'
     url = url.format(tel=mobile)
